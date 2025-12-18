09e07c7b0008d95ef92273cea72fbd0a44ef88b4
sayyadshahid <shahidsayyed0212@gmail.com>
2025-12-18 12:05:33 +0530

    hash table code added

--- DSA/Hash Table/concept.py
+++ DSA/Hash Table/concept.py
@@ -3,3 +3,26 @@
  
 
 
+table_size = 5
+hash_table = [[] for _ in range(table_size)]
+
+def insert(key, value):
+    index = key % table_size
+    hash_table[index].append((key, value))
+    
+
+def search(key):
+    index = key % table_size
+    for k, v in hash_table[index]:
+        if k == key:
+            return v
+    return "Not Found"
+
+insert(12, "A")
+insert(22, "B")
+insert(32, "C")
+
+print(search(20))   # Output: B
+
+
+# A hash table uses a hash function to convert keys into indices. If multiple keys map to the same index, collisions occur, which can be handled using chaining or probing techniques.
