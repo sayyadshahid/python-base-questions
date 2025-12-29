b9c31b89b1f6aa7d82b3c1ab080b9af99f1fa49a
sayyadshahid <shahidsayyed0212@gmail.com>
2025-12-30 00:58:12 +0530

    all questions are solved based on array sorting

--- DSA/Questions/Array/Q10.py
+++ DSA/Questions/Array/Q10.py
@@ -1,1 +1,15 @@
 # 10) Write python program to implement insertion sort for descending order of numbers
+def insertion(arr):
+    n = len(arr)
+    for i in range(1, n):
+        for j in range(i, 0, -1):
+            if arr[j-1] < arr[j]:
+                arr[j-1], arr[j] = arr[j], arr[j-1]
+
+
+arr = [64, 34, 25, 12, 22, 11, 90]
+
+
+insertion(arr)
+
+print( arr)
--- DSA/Questions/Array/Q9.py
+++ DSA/Questions/Array/Q9.py
@@ -1,1 +1,15 @@
 # 9) Write python program to implement bubble sort for descending order of numbers
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n -1):
+        for j in range(n - i -1):
+            if arr[j +1] > arr[j]:
+                arr[j+1], arr[j] = arr[j], arr[j+1]
+
+
+arr = [64, 34, 25, 12, 22, 11, 90]
+
+
+bubble_sort(arr)
+
+print( arr)
