6265bfc9ccb202fe80ef7a8417894efefd07c9c0
sayyadshahid <shahidsayyed0212@gmail.com>
2025-12-30 18:43:39 +0530

    ...

---
+++ DSA/Questions/LinkedList/Q1.py
@@ -0,0 +1,3 @@
+# 1) Write menu driven program for singly list using python which contains option
+# append 2)insert first 3)insert last 4)insert at given position 5)display 6)search by value 7)delete
+# first 8)delete last 9)delete by searching specific value
