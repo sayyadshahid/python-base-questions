a340056ea75a335cd4d7ea94979bb1adac9fa836
Shaikhimran07 <imranshaikh53210@gmail.com>
2026-06-20 00:30:30 +0530

    fix: correct formatting of mid calculation in binary search

--- DSA/Searching/Binary Search/binary-search.py
+++ DSA/Searching/Binary Search/binary-search.py
@@ -3,7 +3,7 @@ def binary_search(arr, x):
     high = len(arr) - 1
 
     while low <= high:
-        mid = (low + high) // 2
+        mid = (low +                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             high) // 2
 
         if arr[mid] == x:
             return mid
